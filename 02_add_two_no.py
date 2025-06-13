5080d7a4d620689f41bd29aa5121bca204805706
Prince Patwa <bajrangipatwa459@gmail.com>
2025-06-13 13:47:34 +0530

    A python program for 'Take two numbers as input and print their sum'

---
+++ 02_add_two_no.py
@@ -0,0 +1,8 @@
+# Take two numbers as input and print their sum.
+
+first_no = int(input("Enter first no. : "))
+second_no = int(input("Enter second no. : "))
+
+sum = first_no + second_no
+
+print("Sum of given no. :",sum)
