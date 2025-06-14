d4490aa79109724e394ace69960f1998617431ef
Prince Patwa <bajrangipatwa459@gmail.com>
2025-06-14 15:02:10 +0530

    Write a program that takes float input and rounds to 2 decimals.

---
+++ 09_rounds_float.py
@@ -0,0 +1,7 @@
+# Write a program that takes float input and rounds to 2 decimals.
+
+num = float(input("Enter a number : "))
+
+round_no = round(num, 2)
+
+print("Rounds no to 2 decimals :",round_no)
