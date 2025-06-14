ee7e6da0c440a01c11cfe5195442619034e789af
Prince Patwa <bajrangipatwa459@gmail.com>
2025-06-14 14:11:47 +0530

    A python program for 'create simple calculator'

---
+++ 04_simple_calculator.py
@@ -0,0 +1,11 @@
+# create simple calculator
+
+firstNO = int(input("Enter the first no : "))    # input the valu of min and change into int
+secondNo = int(input("Enter the second no : "))
+
+print("Sum of given no :",firstNO + secondNo)
+print("Sub of given no :",firstNO - secondNo)
+print("Mul of given no :",firstNO * secondNo)
+print("Div of given no (In int) :",firstNO // secondNo)
+print("Div of given no (In float) :",firstNO / secondNo)
+print("Reminder of given no :",firstNO % secondNo)
