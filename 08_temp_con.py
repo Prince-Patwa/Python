90525bb29a74436aaac4c4c34dbabebc5c51b6af
Prince Patwa <bajrangipatwa459@gmail.com>
2025-06-14 14:50:16 +0530

    A python program for Convert temperature from Celsius to Fahrenheit.

---
+++ 08_temp_con.py
@@ -0,0 +1,7 @@
+# Convert temperature from Celsius to Fahrenheit.
+
+celsius = int(input("Enter the value of temperatuer in celsius : "))
+
+far = celsius * (9/5) + 32
+
+print("Converted value :",far,"fahrenheit")
