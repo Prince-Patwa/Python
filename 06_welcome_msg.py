772b0abfd390b2f618f44d9039de30927f3b4cd7
Prince Patwa <bajrangipatwa459@gmail.com>
2025-06-14 14:33:31 +0530

    A python program for print the welcome msg

---
+++ 06_welcome_msg.py
@@ -0,0 +1,6 @@
+# Take name, age and print: Welcome John, you are 25 years old!
+
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+
+print("Welcome",name,", you are",age,"years old !")
