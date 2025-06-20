21fd89d2864ddeae52cbdeac89edbd845de44183
Prince Patwa <bajrangipatwa459@gmail.com>
2025-06-20 10:03:07 +0530

    Check if a number is prime.

---
+++ 24_prime_number_check.py
@@ -0,0 +1,15 @@
+# Check if a number is prime.
+
+number = int(input("Enter a number: "))
+is_prime = True
+
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            is_prime = False
+            break
+
+if is_prime == True:
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
